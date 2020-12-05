7b3aaf629feaabadc8fb74401034ad2c4e57d88b
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2020-12-05 09:00:59 +0100

    Day 5 simplified

--- src/main/kotlin/nl/rjcoding/aoc2020/Day5.kt
+++ src/main/kotlin/nl/rjcoding/aoc2020/Day5.kt
@@ -2,57 +2,25 @@ package nl.rjcoding.aoc2020
 
 object Day5  : Day {
 
+    val allSeats = (0..1023).toSet()
+
     override fun part1(): Long = Util.readInputToLines("day5.txt")
         .map(::parse)
-        .map { it.id() }
         .maxByOrNull { it }!!.toLong()
 
     override fun part2(): Long = Util.readInputToLines("day5.txt")
-        .map(::parse)
-        .groupBy { it.row.value() }
-        .mapNotNull { (row, seats) ->
-            val usedColumns = seats.map { it.col.value() }
-            val (min, max) = usedColumns.minByOrNull { it }!! to usedColumns.maxByOrNull { it }!!
-            (min .. max).toSet().minus(usedColumns).map { row * 8 + it }.firstOrNull()
-        }.first().toLong()
+        .map(::parse).toSet()
+        .let { occupied -> occupied to allSeats - occupied }
+        .let { (occupied, remaining) ->
+            remaining.first { seatId -> occupied.contains(seatId - 1) && occupied.contains(seatId + 1) }
+        }.toLong()
 
-    fun parse(code: String): Seat = code
-        .asSequence()
-        .fold(Seat.init) { seat, char ->
+    fun parse(code: String): Int = code
+        .map { char ->
             when (char) {
-                'F' -> seat.front()
-                'B' -> seat.back()
-                'L' -> seat.left()
-                'R' -> seat.right()
-                else -> seat
+                'F','L' -> '0'
+                'B','R' -> '1'
+                else -> '0'
             }
-        }
-
-    data class Seat(val row: Region, val col: Region) {
-        fun front(): Seat = copy(row = row.lower())
-        fun back(): Seat = copy(row = row.upper())
-        fun left(): Seat = copy(col = col.lower())
-        fun right(): Seat = copy(col = col.upper())
-
-        fun id(): Int = row.value() * 8 + col.value()
-
-        companion object {
-            val init = Seat(Region.row, Region.col)
-        }
-    }
-
-    data class Region(val min: Int, val max: Int) {
-        fun lower(): Region = copy(max = min + (max - min) / 2)
-        fun upper(): Region = copy(min = min + (max - min + 1) / 2)
-
-        fun value(): Int {
-            require(min == max) { "Value not fully determined" }
-            return min
-        }
-
-        companion object {
-            val row = Region(0, 127)
-            val col = Region(0, 7)
-        }
-    }
+        }.joinToString("").toInt(2)
 }
--- src/test/kotlin/nl/rjcoding/aoc2020/Day5Tests.kt
+++ src/test/kotlin/nl/rjcoding/aoc2020/Day5Tests.kt
@@ -6,57 +6,18 @@ import kotlin.test.assertEquals
 class Day5Tests {
 
     @Test
-    fun rowRegion() {
-        var row = Day5.Region.row
-        assertEquals(0, row.min)
-        assertEquals(127, row.max)
-
-        row = row.lower()
-        assertEquals(0, row.min)
-        assertEquals(63, row.max)
-
-        row = row.upper()
-        assertEquals(32, row.min)
-        assertEquals(63, row.max)
-
-        row = row.lower()
-        assertEquals(32, row.min)
-        assertEquals(47, row.max)
-
-        row = row.upper()
-        assertEquals(40, row.min)
-        assertEquals(47, row.max)
-
-        row = row.upper()
-        assertEquals(44, row.min)
-        assertEquals(47, row.max)
-
-        row = row.lower()
-        assertEquals(44, row.min)
-        assertEquals(45, row.max)
-
-        row = row.lower()
-        assertEquals(44, row.min)
-        assertEquals(44, row.max)
-    }
-
-    @Test
     fun seatId() {
         val seat1 = Day5.parse("FBFBBFFRLR")
-        assertEquals(44, seat1.row.value())
-        assertEquals(5, seat1.col.value())
+        assertEquals(44 * 8 + 5, seat1)
 
         val seat2 = Day5.parse("BFFFBBFRRR")
-        assertEquals(70, seat2.row.value())
-        assertEquals(7, seat2.col.value())
+        assertEquals(70 * 8 + 7, seat2)
 
         val seat3 = Day5.parse("FFFBBBFRRR")
-        assertEquals(14, seat3.row.value())
-        assertEquals(7, seat3.col.value())
+        assertEquals(14 * 8 + 7, seat3)
 
         val seat4 = Day5.parse("BBFFBBFRLL")
-        assertEquals(102, seat4.row.value())
-        assertEquals(4, seat4.col.value())
+        assertEquals(102 * 8 + 4, seat4)
     }
 
 }
