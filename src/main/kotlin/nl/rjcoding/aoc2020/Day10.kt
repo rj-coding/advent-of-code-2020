8e222124346a8582aef8f169c349236a57330814
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2020-12-10 10:29:11 +0100

    Day 10 tweaks

--- src/main/kotlin/nl/rjcoding/aoc2020/Day10.kt
+++ src/main/kotlin/nl/rjcoding/aoc2020/Day10.kt
@@ -4,15 +4,12 @@ object Day10  : Day {
 
     val input get() = Util.readInputToLines("day10.txt").map { it.toInt() }
 
-    override fun part1(): Long = part1(input)
+    override fun part1(): Long = part1(input).toLong()
 
-    fun part1(input: Sequence<Int>) = input.sorted().toList()
-        .let { listOf(0) + it + (it.max()!! + 3) }.asSequence()
+    fun part1(input: Sequence<Int>) = input.plus(0).sorted()
         .windowed(2)
-        .map { (prev, next) -> next - prev }
-        .groupBy { it }
-        .let { it[1]!!.size * it[3]!!.size }
-        .toLong()
+        .groupBy { (prev, next) -> next - prev }
+        .let { it[1]!!.size * (it[3]!!.size + 1)}
 
     override fun part2(): Long = part2(input)
 
